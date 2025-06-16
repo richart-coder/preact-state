d7b33ab087d968ee0a7a9ee1b06b181342dba653
柳澤豐 <liuduofeng@liuduofengde-MacBook-Pro.local>
2025-06-16 16:25:58 +0800

    chore: 統一 SignalObject 類型

--- src/signal/useGlobalSignal.jsx
+++ src/signal/useGlobalSignal.jsx
@@ -1,11 +1,17 @@
 import { useState } from "preact/hooks";
 
 /**
+ * @template T
+ * @typedef {Object} SignalObject
+ * @property {T | null} value - 當前狀態值（getter/setter）
+ */
+
+/**
  * 建立一個全域狀態 Signal，使用 Preact 的 useState 進行狀態管理
  *
  * @template T
  * @param {T} initialValue - 初始值
- * @returns {{ value: T }} 包含 value getter/setter 的 Signal 物件
+ * @returns {SignalObject<T>} 包含 value getter/setter 的 Signal 物件
  */
 function useGlobalSignal(initialValue) {
   const [state, setState] = useState(initialValue);
